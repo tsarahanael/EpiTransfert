79b4bed10a2c16e1a8f8e94f63b8a3c44eb70f44
Stanislas Dolata <stanislas.dolata@epita.fr>
2026-07-07 15:01:38 +0200

    feat(backend): Backend architecture

---
+++ backEnd/src/main/java/org/example/epitransfert/EpiTransfertApplication.java
@@ -0,0 +1,13 @@
+package org.example.epitransfert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiTransfertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpiTransfertApplication.class, args);
+    }
+
+}
